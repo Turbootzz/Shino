d5648a7b8041e9ecd49c215ef81296683a84b7ec
Thijs Herman <tc.herman@hotmail.com>
2022-11-22 23:28:46 +0100

    Set type to mode

--- bot.js
+++ bot.js
@@ -1,8 +1,14 @@
-require("dotenv").config();
+// require("dotenv").config();
+import {} from "dotenv/config";
 
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+// const { Client, GatewayIntentBits } = require('discord.js');
+import { Client, GatewayIntentBits } from 'discord.js';
+// const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: ["Guilds", "GuildMessages"] });
+const PREFIX = "sh!";
 
+
+// listening to ready event, callback event
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -14,5 +20,13 @@ client.on('interactionCreate', async interaction => {
     await interaction.reply('Pong!');
   }
 });
+
+client.on("message", msg => {
+  if (msg.content == "I love shino") {
+    msg.react("❤️")
+  }
+
+
+})
 
 client.login(process.env.BOT_TOKEN);
